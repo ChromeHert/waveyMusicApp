f56be9195d468ec75a67a97cb46a1811072b712c
unknown <ariyeric@gmail.com>
2024-08-06 15:34:13 +0100

    initialized first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-4xl text-slate-400 text-center underline-offset-8'>Music to your ears</h1>
+    </>
+  )
+}
+
+export default App
